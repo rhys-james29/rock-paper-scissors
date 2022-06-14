a258d03f194fb36eb1581b4b81e87ee6e9af8d6e
rhys-james29 <Rhysjamo@gmail.com>
2022-06-14 19:56:49 +0000

    add javascript constants and random number generator

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,20 @@
-console.log("Connected!!")
+const computerChoiceDisplay = document.getElementById('computer-choice')
+const yourChoiceDisplay = document.getElementById('your-choice')
+const resultDisplay = document.getElementById('result')
+const possibleChoices = document.querySelectorAll('button')
+
+let yourChoice
+
+
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+    yourChoice = e.target.id
+    yourChoiceDisplay.innerHTML = yourChoice
+    generateComputerChoice()
+}))
+
+
+function generateComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * possibleChoices.length) + 1
+    console.log(randomNumber)
+    
+  }
