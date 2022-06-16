8d3d4bd448a7c5392b772bf0ca7f27e35050d23d
rhys-james29 <Rhysjamo@gmail.com>
2022-06-16 23:27:29 +0000

    add semicolons missed in java and fix text size for choices on smaller screens

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,35 @@
-const computerChoiceDisplay = document.getElementById('computer-choice')
-const yourChoiceDisplay = document.getElementById('your-choice')
-const resultDisplay = document.getElementById('result')
-const possibleChoices = document.querySelectorAll('button')
-const winsDisplay =  document.getElementById('wins')
-const lossDisplay = document.getElementById('losses')
+const computerChoiceDisplay = document.getElementById('computer-choice');
+const yourChoiceDisplay = document.getElementById('your-choice');
+const resultDisplay = document.getElementById('result');
+const possibleChoices = document.querySelectorAll('button');
+const winsDisplay =  document.getElementById('wins');
+const lossDisplay = document.getElementById('losses');
 
-let yourChoice
-let computerChoice
-let result
-var wins = 0
-var losses = 0
+let yourChoice;
+let computerChoice;
+let result;
+var wins = 0;
+var losses = 0;
 
-winsDisplay.innerHTML = wins
-lossDisplay.innerHTML = losses
+winsDisplay.innerHTML = wins;
+lossDisplay.innerHTML = losses;
 
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
-    yourChoice = e.target.id
-    yourChoiceDisplay.innerHTML = yourChoice
-    generateComputerChoice()
-    getResult()
-    addScore()
+    yourChoice = e.target.id;
+    yourChoiceDisplay.innerHTML = yourChoice;
+    generateComputerChoice();
+    getResult();
+    addScore();
 }))
 
 function addScore(){
   if (result === 'You win!'){
-    wins = wins + 1
-    winsDisplay.innerHTML = wins
+    wins = wins + 1;
+    winsDisplay.innerHTML = wins;
   } else if (result=== 'You lose!'){
-    losses = losses + 1
-    lossDisplay.innerHTML = losses
+    losses = losses + 1;
+    lossDisplay.innerHTML = losses;
   }
 }
  
@@ -38,24 +38,24 @@ function addScore(){
 
 
 function generateComputerChoice() {
-    const randomNumber = Math.floor(Math.random() * possibleChoices.length) + 1
+    const randomNumber = Math.floor(Math.random() * possibleChoices.length) + 1;
      
   if (randomNumber === 1) {
-    computerChoice = 'rock'
+    computerChoice = 'rock';
   }
   if (randomNumber === 2) {
-    computerChoice = 'scissors'
+    computerChoice = 'scissors';
   }
   if (randomNumber === 3) {
-    computerChoice = 'paper'
+    computerChoice = 'paper';
   }
   if (randomNumber === 4) {
-    computerChoice = 'lizard'
+    computerChoice = 'lizard';
   }
   if (randomNumber === 5) {
-    computerChoice = 'spock'
+    computerChoice = 'spock';
   }
-  computerChoiceDisplay.innerHTML = computerChoice
+  computerChoiceDisplay.innerHTML = computerChoice;
      }
 
 
@@ -63,68 +63,68 @@ function generateComputerChoice() {
   function getResult() {
 
     if (computerChoice === yourChoice) {
-      result = 'Draw!'
+      result = 'Draw!';
     }
     if (computerChoice === 'rock' && yourChoice === "paper") {
-      result = 'You win!'
+      result = 'You win!';
     }
     if (computerChoice === 'rock' && yourChoice === "scissors") {
-      result = 'You lose!'
+      result = 'You lose!';
     }
     if (computerChoice === 'rock' && yourChoice === "lizard") {
-        result = 'You lose!'
+        result = 'You lose!';
       }
       if (computerChoice === 'rock' && yourChoice === "spock") {
-        result = 'You win!'
+        result = 'You win!';
       }
     if (computerChoice === 'paper' && yourChoice === "scissors") {
-      result = 'You win!'
+      result = 'You win!';
     }
     if (computerChoice === 'paper' && yourChoice === "rock") {
-      result = 'You lose!'
+      result = 'You lose!';
     }
     if (computerChoice === 'paper' && yourChoice === "lizard") {
-        result = 'You win!'
+        result = 'You win!';
       }
       if (computerChoice === 'paper' && yourChoice === "spock") {
-        result = 'You lose!'
+        result = 'You lose!';
       }
     if (computerChoice === 'scissors' && yourChoice === "rock") {
-      result = 'You win!'
+      result = 'You win!';
     }
     if (computerChoice === 'scissors' && yourChoice === "paper") {
-      result = 'You lose!'
+      result = 'You lose!';
     }
     if (computerChoice === 'scissors' && yourChoice === "lizard") {
-        result = 'You lose!'
+        result = 'You lose!';
       }
     if (computerChoice === 'scissors' && yourChoice === "spock") {
-        result = 'You win!'
+        result = 'You win!';
       }
     if (computerChoice === 'lizard' && yourChoice === "paper") {
-        result = 'You lose!'
+        result = 'You lose!';
       }
     if (computerChoice === 'lizard' && yourChoice === "rock") {
-        result = 'You win!'
+        result = 'You win!';
       }
     if (computerChoice === 'lizard' && yourChoice === "scissors") {
-        result = 'You win!'
+        result = 'You win!';
       }
     if (computerChoice === 'lizard' && yourChoice === "spock") {
-        result = 'You lose!'
+        result = 'You lose!';
       }
     if (computerChoice === 'spock' && yourChoice === "paper") {
-        result = 'You win!'
+        result = 'You win!';
       }
     if (computerChoice === 'spock' && yourChoice === "rock") {
-        result = 'You lose!'
+        result = 'You lose!';
       }
     if (computerChoice === 'spock' && yourChoice === "scissors") {
-        result = 'You lose!'
+        result = 'You lose!';
       }
     if (computerChoice === 'spock' && yourChoice === "lizard") {
-        result = 'You win!'
+        result = 'You win!';
       }
       
-      resultDisplay.innerHTML = result
+      resultDisplay.innerHTML = result;
   }
